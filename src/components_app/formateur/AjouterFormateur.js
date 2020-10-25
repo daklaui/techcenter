6b9065c8c52e93fefccfe0a1d77f038402cb9ecc
dakleuimoataz <daklaoui.mootaz@rades.r-iset.tn>
2020-10-25 21:16:37 +0100

    axios

--- src/components_app/formateur/AjouterFormateur.js
+++ src/components_app/formateur/AjouterFormateur.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import { render } from '@testing-library/react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function AjouterFormateur() {
-    return (
+//add url api
+const api = axios.create({
+  baseURL:''
+});
+
+class Formateur extends Component
+{
+
+
+constructor(props)
+{
+  super(props)
+  this.state={
+    nom:'',
+    prenom :'',
+    email :'',
+    date_de_naissance :'',
+    adresse :'',
+    num_tel :'',
+    mot_de_passe :'',
+    cin :''
+
+  };
+  this.handleChange = this.handleChange.bind(this);
+ // this.handleSubmit = this.handleSubmit.bind(this);
+
+}
+
+handleChange(event) {
+  this.setState({[event.target.name]: event.target.value});
+};
+
+mySubmitHandler = (event) => {
+  event.preventDefault();
+  alert("You are submitting " + this.state.nom);
+console.log(this.state);
+//this.sendDataApi(this.state);
+
+}
+
+// fn send data 
+
+sendDataApi=async(data)=>
+{
+  let res=await api.post('/',data);
+  alert(res);
+}
+
+
+
+
+   
+   
+   
+   render(){
+   
+   return (
 <div>
         <div className="content-header">
         <div className="container-fluid">
@@ -32,64 +89,97 @@ export default function AjouterFormateur() {
           Ajouter Formateur
         </h3>
       </div>
+      <form onSubmit={this.mySubmitHandler}>
       <div className="card-body">
+    
         <div className="row">
           <div className="col-lg-4">
             <div className="form-group">
               <label htmlFor="Hotel">Nom :</label>
-              <input type="text" className="form-control form-control-sm" />
+              <input type="text" 
+                     name="nom" 
+                     className="form-control form-control-sm"
+                     value={this.state.nom}
+                     onChange={this.handleChange}
+                     />
             </div>
           </div>
           <div className="col-lg-4">
             <div className="form-group">
-              <label htmlFor="Hotel">Prenom :</label>
-              <input type="text" className="form-control form-control-sm" />
+              <label >Prenom :</label>
+              <input type="text" 
+                    name="prenom"
+                    className="form-control form-control-sm" 
+                    value={this.state.prenom}
+                    onChange={this.handleChange}
+                    />
             </div>
           </div>
           <div className="col-lg-4">
             <div className="form-group">
-              <label htmlFor="Hotel">Cin :</label>
-              <input type="text" className="form-control form-control-sm" />
+              <label >Cin :</label>
+              <input type="text" name="cin" className="form-control form-control-sm" 
+              value={this.state.cin}
+              onChange={this.handleChange}
+              />
             </div>
           </div>
           <div className="col-lg-4">
             <div className="form-group">
               <label htmlFor="Hotel">Date de naissence :</label>
-              <input type="date" className="form-control form-control-sm" />
+              <input type="date" name="date_de_naissance" className="form-control form-control-sm"
+              value={this.state.date_de_naissance}
+              onChange={this.handleChange}
+              />
             </div>
           </div>
           <div className="col-lg-4">
             <div className="form-group">
               <label htmlFor="Hotel">Téléphone :</label>
-              <input type="number" className="form-control form-control-sm" />
+              <input type="number" name="num_tel" className="form-control form-control-sm" 
+               value={this.state.num_tel}
+               onChange={this.handleChange}
+              />
             </div>
           </div>
           <div className="col-lg-4">
             <div className="form-group">
               <label htmlFor="Hotel">Email :</label>
-              <input type="email" className="form-control form-control-sm" />
+              <input type="email" name="email" className="form-control form-control-sm"
+                 value={this.state.email}
+                 onChange={this.handleChange}
+              />
             </div>
           </div>
           <div className="col-lg-12">
             <div className="form-group">
               <label htmlFor="Hotel">Adresse :</label>
-              <input type="text" className="form-control form-control-sm" />
+              <input type="text" name="adresse" className="form-control form-control-sm"
+               value={this.state.adresse}
+               onChange={this.handleChange}
+              />
             </div>
           </div>
    
           <div className="col-lg-6">
             <div className="form-group">
               <label htmlFor="Hotel">Login :</label>
-              <input type="text" className="form-control form-control-sm" />
+              <input type="text" name="login" className="form-control form-control-sm"
+              
+              />
             </div>
           </div>
           <div className="col-lg-6">
             <div className="form-group">
               <label htmlFor="Hotel">Mot de passe :</label>
-              <input type="password" className="form-control form-control-sm" />
+              <input type="password" name="mot_de_passe" className="form-control form-control-sm"
+              
+              value={this.state.mot_de_passe}
+              onChange={this.handleChange}/>
             </div>
           </div>
         </div>
+   
       </div>
       <div className="card-footer">
       <div class="row">
@@ -98,6 +188,7 @@ export default function AjouterFormateur() {
                 <div class="col-lg-4"></div>
             </div>
       </div>
+      </form>
     </div>
   </div>
   <div className="col-lg-2"></div>
@@ -105,3 +196,7 @@ export default function AjouterFormateur() {
 </div>
     )
 }
+
+};
+
+export default Formateur;
--- src/components_app/formateur/ListeDesFormateurs.js
+++ src/components_app/formateur/ListeDesFormateurs.js
@@ -1,54 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+const api = axios.create({
+  baseURL:''
+});
 
-export default function ListeDesFormateurs() {
-    return (
-        <div>
-            <div className="content-header">
-        <div className="container-fluid">
-          <div className="row mb-2">
-            <div className="col-sm-6">
-              <h1 className="m-0 text-dark">Liste des Formateurs</h1>
-            </div>{/* /.col */}
-            <div className="col-sm-6">
-              <ol className="breadcrumb float-sm-right">
-                <li className="breadcrumb-item"><a href="#">Gestion des formateurs</a></li>
-                <li className="breadcrumb-item active">Liste des formateurs</li>
-              </ol>
-            </div>{/* /.col */}
-          </div>{/* /.row */}
-        </div>{/* /.container-fluid */}
-      </div>
+class ListeDesFormateurs extends Component
+{
+
+constructor()
+{
+  super();
+  api.get('/').then(res=>{
+
+  });
+}
+
+  render(){
+    
+  
+  return (
+    <div>
+        <div className="content-header">
+    <div className="container-fluid">
+      <div className="row mb-2">
+        <div className="col-sm-6">
+          <h1 className="m-0 text-dark">Liste des Formateurs</h1>
+        </div>{/* /.col */}
+        <div className="col-sm-6">
+          <ol className="breadcrumb float-sm-right">
+            <li className="breadcrumb-item"><a href="#">Gestion des formateurs</a></li>
+            <li className="breadcrumb-item active">Liste des formateurs</li>
+          </ol>
+        </div>{/* /.col */}
+      </div>{/* /.row */}
+    </div>{/* /.container-fluid */}
+  </div>
 
 
-      <div className="row">
-    <div className="col-lg-12">
-    <table className="table">
-  <thead>
-    <tr>
-      <th>Cin</th>
-      <th>Nom et Prenom</th>
-      <th>Date de naissence</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>09985214</td>
-      <td>Update software</td>
-      <td>05/06/1999</td>
-      <td>
-          <button type="submit" className="btn btn-info">Voir Details</button>
-          <button type="submit" className="btn btn-warning">Modifier</button>
-          <button type="submit" className="btn btn-danger ">Supprimer</button></td>
-    </tr>
-   
-  </tbody>
+  <div className="row">
+<div className="col-lg-12">
+<table className="table">
+<thead>
+<tr>
+  <th>Cin</th>
+  <th>Nom et Prenom</th>
+  <th>Date de naissence</th>
+  <th>Action</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td>09985214</td>
+  <td>Update software</td>
+  <td>05/06/1999</td>
+  <td>
+      <button type="submit" className="btn btn-info">Voir Details</button>
+      <button type="submit" className="btn btn-warning">Modifier</button>
+      <button type="submit" className="btn btn-danger ">Supprimer</button></td>
+</tr>
+
+</tbody>
 </table>
 
 
-    </div>
-    </div>
+</div>
+</div>
 
-        </div>
-    )
+    </div>
+)
+  }
 }
+
+export default  ListeDesFormateurs;
